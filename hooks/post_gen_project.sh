5f6bfc0d7329c4f7445eddba8b188758947c0e63
pspierce <pspierce@gmail.com>
2016-03-04 09:11:21 -0500

    add sudo to post gen

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -2,6 +2,6 @@
 
 #cd {{cookiecutter.repo_name}}
 docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
-cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
+sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
 cp ./web/requirements.txt ./virtualenv/{{cookiecutter.repo_name}}/
 docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements.txt
