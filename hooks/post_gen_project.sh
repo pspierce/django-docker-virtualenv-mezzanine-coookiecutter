ffcbddc1389a3daefea4cc931247d5d8f1a400d0
pspierce <pspierce@gmail.com>
2016-03-04 10:55:04 -0500

    added in postgres/data

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -4,6 +4,7 @@ set -x
 
 #cd {{cookiecutter.repo_name}}
 echo `pwd`
+rm ./postgresql/data/.test
 docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
 docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements/{{cookiecutter.dev_or_prod}}.txt
