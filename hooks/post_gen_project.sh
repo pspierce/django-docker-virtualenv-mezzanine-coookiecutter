37c06f87e624aae7af841eb0c4747f90057d5557
pspierce <pspierce@gmail.com>
2016-03-04 11:51:30 -0500

    post gen update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -8,3 +8,8 @@ rm ./postgresql/data/.test
 docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
 docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements/{{cookiecutter.dev_or_prod}}.txt
+docker-compose run --rm postgres sh -c 'exec createdb -U postgres -h "$POSTGRES_PORT_5432_TCP_ADDR" {{cookiecutter.repo_name}}';
+docker-compose run --rm web activate.sh ./manage.py migrate
+docker-compose run --rm web activate.sh ./manage.py createsuperuser
+docker-compose run --rm web activate.sh ./manage.py collectstatic
+docker-compose run --rm web activate.sh ./manage.py startapp {{cookiecutter.repo_name}}
