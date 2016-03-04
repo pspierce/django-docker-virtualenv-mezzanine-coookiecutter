906f2d1df71727b9d195367e508b199996dbf3c8
pspierce <pspierce@gmail.com>
2016-03-04 09:46:22 -0500

    Moved web's repo stuff to the virtualenv

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -6,5 +6,4 @@ set -x
 echo `pwd`
 docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
-cp ./web/requirements.txt ./virtualenv/{{cookiecutter.repo_name}}/
-docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements.txt
+docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements/{{cookiecutter.dev_or_prod}}.txt
