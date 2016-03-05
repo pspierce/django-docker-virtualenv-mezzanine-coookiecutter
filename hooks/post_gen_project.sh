e97a87d6cf7d53ab28159cfbd4e98d7932efde8a
Scott Pierce <pspierce@gmail.com>
2016-03-05 14:51:19 -0500

    docker-entrypoint-initdb.d

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -10,6 +10,13 @@ docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
 docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements/{{cookiecutter.dev_or_prod}}.txt
 docker-compose up -d postgres
+
+echo "Continue [Y/n]?"
+read continue
+
+docker-compose stop postgres
+docker-compose up -d postgres
+
 sed -i '/host all all 127.0.0.1\/32 trust/a host all all 172.17.0.1\/16 trust' /var/lib/postgresql/data/pg_hba.conf
 docker-compose stop postgres
 docker-compose run --rm postgres sh -c 'exec createdb -U postgres -h "$POSTGRES_PORT_5432_TCP_ADDR" {{cookiecutter.repo_name}}';
