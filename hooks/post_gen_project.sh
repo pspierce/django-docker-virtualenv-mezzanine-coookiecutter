fcda43b9df542a6c3e333138e63e9c71a4dbcf50
pspierce <pspierce@gmail.com>
2016-03-07 15:02:08 -0500

    post gen update

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -5,14 +5,14 @@ set -x
 #cd {{cookiecutter.repo_name}}
 cwd=$(pwd)
 rm $cwd/postgresql/data/.test
-docker-compose build web
 docker-compose up -d postgres
 docker-compose stop postgres
 sudo echo "host all  all    0.0.0.0/0  md5" >> $cwd/postgresql/data/pg_hba.conf
 sudo echo "listen_addresses='*'" >> $cwd/postgresql/data/postgresql.conf
 docker-compose up -d postgres
-docker-compose run --rm postgres sh -c 'exec createdb -U postgres -h "$POSTGRES_PORT_5432_TCP_ADDR" {{cookiecutter.repo_name}}';
+docker-compose run --rm postgres sh -c 'exec createdb -U postgres -h localhost {{cookiecutter.repo_name}}';
 
+docker-compose build web
 docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}/bin/
 docker-compose run --rm web activate.sh pip install -r  /virtualenv/{{cookiecutter.repo_name}}/requirements/{{cookiecutter.dev_or_prod}}.txt
