25755dd381007c02962f561a1a4a67d2d9cf0715
pspierce <pspierce@gmail.com>
2016-03-10 10:23:05 -0500

    post gen update

--- {{ cookiecutter.repo_name }}/docker-entrypoint-initdb.d/setup-db.sh
+++ {{ cookiecutter.repo_name }}/docker-entrypoint-initdb.d/setup-db.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-echo "host all  all    0.0.0.0/0  md5" | sudo tee -a /var/lib/postgresql/data/pg_hba.conf
-echo "listen_addresses='*'" | sudo tee -a /var/lib/postgresql/data/postgresql.conf
+echo "host all  all    0.0.0.0/0  md5" | tee -a /var/lib/postgresql/data/pg_hba.conf
+echo "listen_addresses='*'" | tee -a /var/lib/postgresql/data/postgresql.conf
