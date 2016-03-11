36b0625f2b27cf52181ec142bd36f81282ac7f87
Scott Pierce <pspierce@gmail.com>
2016-03-10 21:33:13 -0500

    added pg_ctl stop to init db script

--- {{ cookiecutter.repo_name }}/docker-entrypoint-initdb.d/setup-db.sh
+++ {{ cookiecutter.repo_name }}/docker-entrypoint-initdb.d/setup-db.sh
@@ -2,3 +2,4 @@
 
 echo "host all  all    0.0.0.0/0  md5" | tee -a /var/lib/postgresql/data/pg_hba.conf
 echo "listen_addresses='*'" | tee -a /var/lib/postgresql/data/postgresql.conf
+su postgres -c "/usr/lib/postgresql/9.5/bin/pg_ctl stop"
