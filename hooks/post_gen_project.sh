32e9a877dd79b4b116b146d29c7b0b9cc40e1e98
Scott Pierce <pspierce@gmail.com>
2016-03-16 21:37:45 -0400

    Patched up post_gen to get new path

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -23,12 +23,11 @@ docker-compose run --rm web virtualenv /virtualenv/{{cookiecutter.repo_name}}env
 sudo cp web/activate.sh ./virtualenv/{{cookiecutter.repo_name}}env/bin/
 docker-compose run --rm web activate.sh pip install -r /webfiles/requirements/{{cookiecutter.dev_or_prod}}.txt
 docker-compose run --rm web activate.sh mezzanine-project {{cookiecutter.repo_name}}site
-sudo mv ./web/sass ./web/static ./web/templates ./virtualenv/{{cookiecutter.repo_name}}env/{{cookiecutter.repo_name}}site/
+sudo mv ./web/sass ./web/static ./web/templates ./virtualenv/{{cookiecutter.repo_name}}/{{cookiecutter.repo_name}}site/
 read command
 docker-compose run --rm web activate.sh ./manage.py migrate
 docker-compose run --rm web activate.sh ./manage.py createsuperuser
 docker-compose run --rm web activate.sh ./manage.py collectstatic
-docker-compose run --rm web activate.sh ./manage.py  {{cookiecutter.app_name}}
 docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock --name nginx-proxy jwilder/nginx-proxy
 echo "host all  all    0.0.0.0/0  md5" | sudo tee -a $cwd/postgresql/data/pg_hba.conf
 docker-compose stop
