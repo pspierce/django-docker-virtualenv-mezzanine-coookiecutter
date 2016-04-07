1cd80f63c09567abffa39d1dcfc619ae090e768b
Scott Pierce <pspierce@gmail.com>
2016-04-07 06:24:03 -0400

    update

---
+++ {{ cookiecutter.repo_name }}/web/local_mezz_settings.py
@@ -0,0 +1,53 @@
+# This file is exec'd from settings.py, so it has access to and can
+# modify all the variables in settings.py.
+
+# If this file is changed in development, the development server will
+# have to be manually restarted because changes will not be noticed
+# immediately.
+
+DEBUG = True
+
+# Make these unique, and don't share it with anybody.
+SECRET_KEY = "{{cookiecutter.django_secret_key}}"
+NEVERCACHE_KEY = "f0v&w=&&0sndh+q3$+_gaznj=t5o1#@1u(03#a$vny_xynwzaq"
+
+DATABASES = {
+    "default": {
+        # Add "postgresql_psycopg2", "mysql", "sqlite3" or "oracle".
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        # DB name or path to database file if using sqlite3.
+        "NAME": "{{cookiecutter.repo_name}}",
+        # Not used with sqlite3.
+        "USER": "{{cookiecutter.repo_name}}_user",
+        # Not used with sqlite3.
+        "PASSWORD": "{{cookiecutter.postgres_password}}",
+        # Set to empty string for localhost. Not used with sqlite3.
+        "HOST": "127.0.0.1",
+        # Set to empty string for default. Not used with sqlite3.
+        "PORT": "",
+    }
+}
+
+
+###################
+# DEPLOY SETTINGS #
+###################
+
+# Domains for public site
+ALLOWED_HOSTS = [""]
+
+# These settings are used by the default fabfile.py provided.
+# Check fabfile.py for defaults.
+
+# FABRIC = {
+#     "DEPLOY_TOOL": "rsync",  # Deploy with "git", "hg", or "rsync"
+#     "SSH_USER": "",  # VPS SSH username
+#     "HOSTS": [""],  # The IP address of your VPS
+#     "DOMAINS": ALLOWED_HOSTS,  # Edit domains in ALLOWED_HOSTS
+#     "REQUIREMENTS_PATH": "requirements.txt",  # Project's pip requirements
+#     "LOCALE": "en_US.UTF-8",  # Should end with ".UTF-8"
+#     "DB_PASS": "",  # Live database password
+#     "ADMIN_PASS": "",  # Live admin user password
+#     "SECRET_KEY": SECRET_KEY,
+#     "NEVERCACHE_KEY": NEVERCACHE_KEY,
+# }
